d9008961066efcb2374e7c03d6418462790f3fdf
Yoris <yorisprayogo@gmail.com>
2016-09-08 18:09:15 +0700

    add insertternakactivity

--- app/src/main/java/com/fintech/ternaku/InsertTernakActivity.java
+++ app/src/main/java/com/fintech/ternaku/InsertTernakActivity.java
@@ -94,8 +94,7 @@ public class InsertTernakActivity extends AppCompatActivity {
 
         final InputMethodManager imgr = (InputMethodManager) InsertTernakActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
         imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
-
-
+        int z=0;
         // setup a dialog window
         alertDialogBuilder.setCancelable(false)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
